de4faeb4c2610a05936e4d94f649620302c9f347
Julia Nowicka <j.nowicka.98@gmail.com>
2022-10-25 17:16:58 +0100

    routing2

--- my-app/backend/routes/notes.js
+++ my-app/backend/routes/notes.js
@@ -23,5 +23,27 @@ router.route('/add-note').post((req, res) => {
     .catch(err => res.status(err).json('Error: ' + err))
     })
 
-    module.exports = router
+router.route('/:id').delete((req, res) => {
+    Note.findByIdAndDelete(req.params.id)
+    .then(notes => res.json('Exercise deleted!'))
+    .catch(err => res.status(err).json('Error ' + err))
+})
+
+router.route('/update/:id').post((req, res) =>{
+    Note.findById(req.params.id)
+    .then(note => {
+        note.category = req.body.category
+        note.title = req.body.title
+        note.content = req.body.content
+
+        note.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error ' + err))
+    })
+    .catch(err => res.status(400).json('Error ' + err)) 
+})
+
+module.exports = router
+
+
 
