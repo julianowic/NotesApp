334e429ddc1b442468519fdddfa2029e3c343fca
Julia Nowicka <j.nowicka.98@gmail.com>
2022-10-31 11:58:30 +0000

    clean up

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -5,21 +5,6 @@ import { BrowserRouter, Routes, Route} from "react-router-dom"
 import axios from "axios"
 
 function App() {
-  // const [notes, setNotes] = useState([]);
-  // const [categories, setCategories] = useState([]);
-
-  // useEffect(() => {
-  //   fetch('http://localhost:5000/notes')
-  //   .then(res => res.json())
-  // .then(json => {
-  //   setNotes(json)})
-  // }, [])
-
-  // useEffect(() => {
-  //   fetch('http://localhost:5000/categories')
-  //   .then(res => res.json())
-  //   .then(json => setCategories(json))
-  // }, [])
 
   return (
     <div>
@@ -27,9 +12,6 @@ function App() {
     <div className="container"> 
       <Routes>
       <Route path="/" element={<MainArea/>} exact/>
-      {/* <Route path="/edit/:id" element={<Note/>}/>
-      <Route path="/create" element={<CreateArea/>}/> */}
-      {/* <Route path="/add-category" element={<Category/>}/> */}
       </Routes>
     </div>
     </BrowserRouter>
--- my-app/src/components/ListCategories.js
+++ my-app/src/components/ListCategories.js
@@ -4,7 +4,7 @@ import CreateCategory from "./CreateCategory";
 
 export default function ListCategories(props) {
     return (
-        <div>
+        <div className="category-group">
         <CreateCategory/>
         <div className="btn-group">
         {props.categories.map((categoryItem, index) =>{
