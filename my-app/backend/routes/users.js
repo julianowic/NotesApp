4850fadb7b83d0494bde99e6f8e795265ee645be
Julia Nowicka <j.nowicka.98@gmail.com>
2022-11-20 14:37:18 +0000

    add user

--- my-app/backend/routes/users.js
+++ my-app/backend/routes/users.js
@@ -20,7 +20,45 @@ let User = require('../models/user.model')
 //     .catch(err => res.status(err).json('Error: ' + err))
 //     })
 const registerUser = asyncHandler(async (req, res) => {
-    res.json("registerUser")
+    const {name, email, password} = req.body
+
+    //validate
+
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error("Please add all fields")
+    }
+
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error("User already exists")
+    }
+
+    //hash password
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //create new user
+
+    const user = await User.create({
+        name : name, 
+        email : email, 
+        password : hashedPassword
+    })
+
+    if (user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email 
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
 })
 
 const loginUser = asyncHandler(async (req, res) => {
